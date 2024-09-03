71a7fd1654a8a5649abfffc65a5605e5ae2a2f2b
Oluwatobiloba Odeyemi <petertob01@gmail.com>
2024-09-03 15:41:40 +0100

    initial commit

---
+++ deal-drop/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+function App() {
+  return (
+    <div className="App">
+       <h1 className="text-success">GeeksforGeeks</h1>
+            <div className="bg-info p-5">
+                <p className="">
+                    This is example for using button with
+                    bootstrap styling
+                </p>
+                <a
+                    className="btn btn-primary"
+                    data-bs-toggle="collapse"
+                    href="#collapseExample"
+                    role="button"
+                    aria-expanded="false"
+                    aria-controls="collapseExample"
+                >
+                    Bootstrap button
+                </a>
+            </div>
+    </div>
+  );
+}
+
+export default App;
